f83bdca4b16972e37612fdfbab8827e817432f8a
Mike Sowter <mike@sowter.com>
2024-09-11 13:27:31 +1000

    found error in energy calcs

--- src/energy.cpp
+++ src/energy.cpp
@@ -6,6 +6,7 @@
 float noise[] = {5,5,5,5,5,5,50,5,5};  // updated 20220725 to handle oven(6) noise
 
 // Energy sums are reset at midnight in minProc
+// all energy is expressed in kWh, so 1s increments are very small (>4E-6)
 
 void dailyEnergy() {
 
@@ -47,6 +48,8 @@ void dailyEnergy() {
       spareSolar = 0.0F;
     }
     // calculate tier1 costs
+    split = tier1solar/tier1loads;  
+    rate = FIT * split + T11 * (1.0F - split);        // (2,3,4,6,8) are essential
     for ( int i = 2;i<NUM_CCTS+1;i++ ) {
       if ( i == 5 && waterOn ) {
         costEnergy[ps][i] += T31 * incEnergy[5];      // hotwater tariff
@@ -55,17 +58,15 @@ void dailyEnergy() {
         costEnergy[ps][7] += FIT * spareSolar;        // export unuseable solar
       }
       else {
-        split = tier1solar/tier1loads;                // loads metered separately
-        rate = FIT * split + T11 * (1.0F - split);    // (2,3,4,6,8) are essential
         costEnergy[ps][i] += rate * incEnergy[i];     // use first portion of solar
       }
     }
     T11_inc[ps] = tier1loads - tier1solar; 
     // calculate tier2 costs
-    if ( tier2loads > 4E-6 ) {                        // loads lumped together
+    if ( tier2loads > 4E-6 ) {                        // loads lumped together >5W
       split = tier2solar/tier2loads;                  // use second portion of solar
       rate = FIT * split + T11 * (1.0F - split);
-      costEnergy[ps][1] += rate * tier2loads;
+      costEnergy[ps][1] += rate * tier2loads;         // cost of total load
       T11_inc[ps] += (tier2loads - tier2solar);
     }
     T11_kWh[ps] += T11_inc[ps];
@@ -74,8 +75,8 @@ void dailyEnergy() {
   }
   // this is a power calc, not energy, for debugging purposes only
   T11_W = max(0.0F,Wrms_min[1] - Wrms_min[7]);
-  if ( T11_W > 500.0F && logon ) {
-    sprintf(charBuf,"LD-%.f T1-%.f T2-%.f SS-%.f T11-%.f ms-%d"
+  if ( T11_W > 1000.0F && logon ) {
+    sprintf(charBuf,"LD-%.6f T1-%.6f T2-%.6f SS-%.6f T11-%.6f ms-%d"
             ,loads,tier1loads,tier2loads,spareSolar,T11_W,t_scan);
     diagMess(charBuf);
     logon = false;
