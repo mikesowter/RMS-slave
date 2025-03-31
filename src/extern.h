2b792e23827c8fd044afc5156eb9604a9222c837
Mike Sowter <mike@sowter.com>
2025-03-31 21:14:05 +1000

    replace FTP with SimpleFTPServer

--- src/extern.h
+++ src/extern.h
@@ -5,7 +5,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266FtpServer.h>
+#include <SimpleFTPServer.h>
 #include <WiFiudp.h>
 #include <LittleFS.h>
 #include <ESP8266mDNS.h>
